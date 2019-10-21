d8533afe948e4921c635aa68fc1f53d1bd8201aa
godD <fgsfsjf@qq.com>
2019-10-21 10:43:50 +0800

    第二天第一部分
    模拟画图

--- Main.js
+++ Main.js
@@ -1,6 +1,7 @@
 //'程序的主类,用于小程序过程中的初始化,以及点击事件的绑定
 
 import { ResourceLoader } from "./js/base/ResourceLoader.js";
+import { DataStore } from "./js/base/DataStore.js";
 
 
 export class Main{
@@ -11,12 +12,30 @@ export class Main{
         this.ctx=this.canvas.getContext('2d');
         //初始化资源加载器
         this.loader=new ResourceLoader();
+        //初始化变量池
+        this.datastore=DataStore.getInstance();
+
         //加载完成后,执行其他的操作
         this.loader.onloaded(map=>this.onResourceLoaded(map));
 
     }
     //资源加载完成后执行其他操作的方法
     onResourceLoaded(map){
-        console.log(map);
+        // console.log(map);
+        // 模拟画背景图
+        // let bg=map.get('background');//拿背景图
+        // this.ctx.drawImage(bg,0,0,bg.width,bg.height,0,0,this.canvas.width,this.canvas.height);
+        //保存各种资源
+        //不使用set方法保存的原因,set方法保存的数据在用细结束时,会被销毁,而下面的数据即使用细结束也不会销毁,下一局继续使用
+        this.datastore.canvas=this.canvas;
+        this.datastore.ctx=this.ctx;
+        this.datastore.res=map;
+
+        this.init();
+     
+    }
+    //游戏初始化
+    init(){
+
     }
 }
---
+++ js/base/Sprite.js
@@ -0,0 +1,33 @@
+import { DataStore } from "./DataStore.js";
+
+//各种图片的父类
+
+export class Sprite{
+    constructor(img=null,srcX=0,srcY=0,srcW=0,srcH=0,x=0,y=0,width=0,height=0){
+        //获取变量池
+        const dataStory=DataStore.getInstance();
+        this.ctx=dataStory.ctx;
+        this.img=img;//图片
+        this.srcX=srcX;//图片的起始x位置
+        this.srcY=srcY;//图片的起始y位置
+        this.srcW=srcW;//图片的宽
+        this.srcH=srcH;//图片的高
+        this.x=x;//在画布上x的坐标
+        this.y=y;//有的坐标
+        this.width=width;//画的宽
+        this.height=height;//画的高
+    }
+
+    //画图
+    draw(img=this.img,srcX=this.srcX,srcY=this.srcY,srcW=this.srcW,srcH=this.srcH,x=this.x,y=this.y,width=this.width,height=this.height){
+        this.ctx.drawImage(img,srcX,srcY,srcW,srcH,x,y,width,height);
+    }
+
+    //获取指定图片
+    static getImage(key){
+        return DataStore.getInstance().res.get(key);
+    }
+       
+
+    
+}
